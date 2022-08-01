12581c1aaf335115482b9c5586827d05be2605e2
Alexei Shcherbakov <home@local>
2022-08-01 22:34:27 +0300

    sqlite-netFx-source-1.0.116.0

---
+++ sqlite-netfx-source/SQLite.Interop/src/generic/interop.h
@@ -0,0 +1,18 @@
+/*
+ * interop.h -
+ *
+ * Written by Joe Mistachkin.
+ * Released to the public domain, use at your own risk!
+ */
+
+#ifndef INTEROP_VERSION
+#define INTEROP_VERSION          "1.0.116.0"
+#endif
+
+#ifndef INTEROP_SOURCE_ID
+#define INTEROP_SOURCE_ID        "e76de45fffbb6ab51b1d72269e16a7f53f775827"
+#endif
+
+#ifndef INTEROP_SOURCE_TIMESTAMP
+#define INTEROP_SOURCE_TIMESTAMP "2022-06-01 14:28:30 UTC"
+#endif
---
+++ sqlite-netfx-source/System.Data.SQLite/SQLitePatchLevel.cs
@@ -0,0 +1,16 @@
+/********************************************************
+ * ADO.NET 2.0 Data Provider for SQLite Version 3.X
+ * Written by Joe Mistachkin (joe@mistachkin.com)
+ *
+ * Released to the public domain, use at your own risk!
+ ********************************************************/
+
+using System.Data.SQLite;
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceId("e76de45fffbb6ab51b1d72269e16a7f53f775827")]
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceTimeStamp("2022-06-01 14:28:30 UTC")]
